ab339c74f824a40846254b7fbd5282182ef86f19
Bora Karaca <bora.karaca@ogr.iu.edu.tr>
2024-05-18 15:13:47 +0300

    Add initial project files

---
+++ index.ts
@@ -0,0 +1,15 @@
+import { Hono } from "hono";
+import { cors } from "hono/cors";
+
+const app = new Hono();
+
+app.use(cors());
+
+app.post("/", async (c) => {
+  if (c.req.header("Content-Type") !== "application/json") {
+    return c.json({ error: "JSON body expected" }, 406);
+  }
+
+  try {
+  } catch (error) {}
+});
---
+++ seed.ts
@@ -0,0 +1,47 @@
+import fs from "fs";
+import csv from "csv-parser";
+import { Index } from "@upstash/vector";
+
+const index = new Index({
+  url: process.env.UPSTASH_VECTOR_URL,
+  token: process.env.UPSTASH_VECTOR_TOKEN,
+});
+
+type Row = {
+  text: string;
+};
+
+async function parseCSV(filePath: string): Promise<Row[]> {
+  return new Promise((resolve, reject) => {
+    const rows: Row[] = [];
+
+    fs.createReadStream(filePath).pipe(
+      csv({ separator: "," })
+        .on("data", (row) => {
+          rows.push(row);
+        })
+        .on("error", (error) => reject(error))
+        .on("end", () => resolve(rows))
+    );
+  });
+}
+
+const STEP = 30;
+
+const seed = async () => {
+  const data = await parseCSV("./trainning_dataset.csv");
+
+  for (let i = 0; i < data.length; i += STEP) {
+    const chunk = data.slice(i, i + STEP);
+
+    const formatted = chunk.map((row, batchIndex) => ({
+      data: row.text,
+      id: i + batchIndex,
+      metadata: { text: row.text },
+    }));
+
+    await index.upsert(formatted);
+  }
+};
+
+seed();
